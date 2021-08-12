83d4bf76733d0978cb4654e6a0d3356ac2d48c26
Matheus Costa <matheus20costa@hotmail.com>
2021-08-12 13:01:05 -0300

    Ajustes no banco
    Ajustei algumas tabelas do banco, que não batiam com a ideia final do sistema.

--- backend/banco/createAlunoETutores.sql
+++ backend/banco/createAlunoETutores.sql
@@ -1,6 +1,6 @@
 drop table if exists aulacoletiva CASCADE;
 drop table if exists usuario CASCADE;
-drop table if exists avaliacao CASCADE;
+drop table if exists avaliacao_aulacoletiva CASCADE;
 drop table if exists aulaindividual CASCADE;
 drop table if exists alunos_aulacoletiva CASCADE;
 drop table if exists conteudo CASCADE;
@@ -11,6 +11,8 @@ drop table if exists inscricao CASCADE;
 drop table if exists instituicao CASCADE;
 drop table if exists tutor_recebe CASCADE;
 drop table if exists transacao CASCADE;
+drop table if exists avaliacao_aulaindividual CASCADE;
+
 
 
 create table if not exists USUARIO (
@@ -22,7 +24,9 @@ create table if not exists USUARIO (
 	senha Varchar(20),
 	endereco int,
 	tipo int,
-	formacao VARCHAR(100)
+	formacao VARCHAR(100),
+	foto VARCHAR(500)
+	
 );
 
 create table if not exists ENDERECO (
@@ -61,18 +65,19 @@ create table if not exists INSTITUICAO (
 
 create table if not exists AULAINDIVIDUAL (
 	aulaIndividual_id serial not null primary key,
-	dataAulaIndividual DATE,
+	dataAulaIndividual timestamp with time zone,
 	status int,
 	comentario VARCHAR(500),
 	aluno int,
-	conteudo int
+	conteudo int,
+	duracao time
 );
 
 Create TABLE if not EXISTS TRANSACAO (
 	transacao_id serial not null primary key,
     tipo varchar(60),
     valor float,
-    dataTransacao date,
+    dataTransacao timestamp with time zone,
     status varchar(60),
     usuario int,
     participante int
@@ -97,7 +102,7 @@ CREATE TABLE IF NOT EXISTS avaliacao_aulacoletiva(
 
 create table if not exists Inscricao (
     inscricao_id serial not null primary key,
-    dataInscrição date,
+    dataInscrição timestamp with time zone,
     aluno int,
     tutor int
 	
@@ -106,10 +111,11 @@ create table if not exists Inscricao (
 
 create table if not exists AulaColetiva (
     aulaColetiva_id serial not null primary key,
-    dataAulaColetiva date,
+    dataAulaColetiva timestamp with time zone,
     status int,
     descricao VARCHAR(500),
-    conteudo int
+    conteudo int,
+    duracao time
     
 );
 
@@ -136,7 +142,7 @@ alter table AULAINDIVIDUAL add foreign key (conteudo) references CONTEUDO (conte
 alter table transacao add foreign key (usuario) references Usuario (usuario_id);
 alter table transacao add foreign key (participante) references Usuario (usuario_id);
 alter table avaliacao_aulaindividual add foreign key (aula) references AULAINDIVIDUAL (aulaIndividual_id);
-alter table avaliacao_aulacoletiva add foreign key (aula) references AULAINDIVIDUAL (aulaIndividual_id);
+alter table avaliacao_aulacoletiva add foreign key (aula) references aulacoletiva (aulacoletiva_id);
 alter table avaliacao_aulacoletiva add foreign key (aluno) references Usuario (usuario_id);
 alter table INSCRICAO add foreign key (aluno) references Usuario (usuario_id);
 alter table INSCRICAO add foreign key (tutor) references Usuario (usuario_id);
@@ -157,13 +163,17 @@ insert into endereco (cep, pais, estado, cidade, bairro, rua, numero) values
 select * from endereco e;
 
 insert into usuario (nome, cpf, datanascimento, email, senha, endereco, tipo, formacao) values
-	('Matheus Costa', '1111', '1996/08/20', 'matheus@gmail.com', 'matheus123', 1 , 2, 'Ensino Superior completo' ),
-	('Pedro Guimarães', '2222', '1988/11/10', 'pedro@gmail.com', 'pedro123', 2 , 1, 'Ensino medio completo' ),
-	('Alice Santos', '3333', '1999/03/21', 'alice@gmail.com', 'alice123', 4 , 1, 'Cursando Mestrado' ),
-	('Ana Luiza', '4444', '2002/01/01', 'ana@gmail.com', 'luiza123', 5, 2, 'Ensino superior completo' ),
-	('João Silva', '5555', '2002/10/14', 'joao@gmail.com', 'joao123', 3, 1, 'Cursando Ensino Superior' );
+	('Matheus Costa', '1111', '1996/08/20', 'matheus@gmail.com', 'matheus123', 1 , 2, 'Ensino Superior completo', 'assets/avatar_tutor.png' ),
+	('Pedro Guimarães', '2222', '1988/11/10', 'pedro@gmail.com', 'pedro123', 2 , 1, 'Ensino medio completo', 'assets/avatar_aluno.png' ),
+	('Alice Santos', '3333', '1999/03/21', 'alice@gmail.com', 'alice123', 4 , 1, 'Cursando Mestrado', 'assets/avatar_aluno.png' ),
+	('Ana Luiza', '4444', '2002/01/01', 'ana@gmail.com', 'luiza123', 5, 2, 'Ensino superior completo', 'assets/avatar_tutor.png' ),
+	('João Silva', '5555', '2002/10/14', 'joao@gmail.com', 'joao123', 3, 1, 'Cursando Ensino Superior', 'assets/avatar_aluno.png' );
 
 select * from usuario u;
+
+UPDATE usuario 
+	SET foto = 'assets/avatar_aluno.jpg'
+	where tipo = 1;
 
 insert into instituicao (nome) values 
 	('IFES'), ('UFES'), ('FAESA'), ('Centro de Linguas');
@@ -171,30 +181,36 @@ insert into instituicao (nome) values
 select * from instituicao i;
 
 insert into conteudo (valor, nivel, instituicao, area_dominio, tutor_id) values
-	(100.00, 5, 2, 'Algebra linear', 1),
-	(150.00, 3, 2, 'TPA', 1),
-	(300.00, 5, 5, 'Ingles', 1),
-	(200.00, 4, 5, 'Alemão', 4),
+	(100.00, 5, 1, 'Algebra linear', 1),
+	(150.00, 3, 1, 'TPA', 1),
+	(300.00, 5, 4, 'Ingles', 1),
+	(200.00, 4, 4, 'Alemão', 4),
 	(50.00, 3, 3, 'Arqueologia', 4),
-	(140.00, 5, 3, 'Biologia', 1);
+	(140.00, 5, 2, 'Biologia', 1);
 
 select * from conteudo c;
 
 insert into inscricao (datainscrição, aluno, tutor) values 
-	('2021/07/20', 2, 1),
-	('2021/06/10', 2, 4),
-	('2021/07/20', 3, 1),
-	('2021/08/01', 5, 4),
-	('2021/05/28', 1, 4);
+	('2021/07/20 22:10', 2, 1),
+	('2021/06/10 23:50', 2, 4),
+	('2021/07/20 17:21', 3, 1),
+	('2021/08/01 18:22', 5, 4),
+	('2021/05/28 09:00', 1, 4);
 
 select * from inscricao i;
 
-insert into aulaindividual (dataaulaindividual, status, comentario, aluno, conteudo) values
-	('2021/07/20', 3, 'preciso de ajuda para entender melhor os conceitos basicos de Algebra', 2, 25),
-	('2021/08/10', 1, 'verb to be', 5, 27),
-	('2021/08/02', 4, 'Covid-19', 3, 30);
+insert into aulaindividual (dataaulaindividual, status, comentario, aluno, conteudo, duracao) values
+	('2021/07/20 13:00', 3, 'preciso de ajuda para entender melhor os conceitos basicos de Algebra', 2, 1, '01:00:00'),
+	('2021/08/10 14:30', 1, 'verb to be', 5, 3, '01:00:00'),
+	('2021/08/02 16:00', 4, 'Covid-19', 3, 6, '01:00:00');
+
+insert into aulaindividual (dataaulaindividual, status, comentario, aluno, conteudo, duracao) values
+	('2021/07/20 13:00', 1, 'preciso de ajuda para entender melhor os conceitos basicos de Algebra', 2, 1, '01:00:00'),
+	('2021/08/10 15:40', 1, 'teste', 5, 3, '01:00:00');
+insert into aulaindividual (dataaulaindividual, status, comentario, aluno, conteudo, duracao) values
+	('2021/08/20 19:00', 1, 'teste', 5, 3, '01:00:00');
 	
-select * from aulaindividual;
+select * from aulaindividual where aulaindividual_id = 2;
 
 insert into discinteresse (nivel, disciplina, usuario) values
 	('Superior', 'Algebra Linear', 1),
@@ -205,11 +221,11 @@ insert into discinteresse (nivel, disciplina, usuario) values
 	
 select * from discinteresse;
 
-insert into aulacoletiva (dataaulacoletiva, descricao, conteudo, status) values
-	('2021/08/12', 'Basico de ingles', 27, 1),
-	('2021/08/10', 'Biologia basica', 30, 2),
-	('2021/08/10', 'Apresentando meu conhecimento em Algebra Linear', 25, 4),
-	('2021/08/5', 'Alemão basico', 28, 3);
+insert into aulacoletiva (dataaulacoletiva, descricao, conteudo, status, duracao) values
+	('2021/08/12 19:00', 'Basico de ingles', 3, 1, '01:00:00'),
+	('2021/08/10 18:00', 'Biologia basica', 6, 2, '01:00:00'),
+	('2021/08/10 15:00', 'Apresentando meu conhecimento em Algebra Linear', 1, 4, '01:00:00'),
+	('2021/08/5 13:00', 'Alemão basico', 4, 3, '01:00:00');
 	
 	
 select * from aulacoletiva;
@@ -229,7 +245,7 @@ insert into alunos_aulacoletiva (aulacoletiva, aluno) values
 select * from alunos_aulacoletiva aa;
 	
 insert into avaliacao_aulaindividual (comentario, nota, aula) values 
-	('muito bom', 5, 4);
+	('muito bom', 5, 1);
 	
 	
 select * from avaliacao_aulaindividual;
@@ -248,9 +264,12 @@ insert into dataindisponivel (dataindisponivel, tutor) values
 select * from dataindisponivel d;
 
 insert into transacao (tipo, valor, datatransacao, status, usuario, participante) values
-	('Deposito', 300.00, '2021/06/10', 'Em aprovação', 2, null),
-	('Deposito', 500.00, '2021/06/02', 'Aprovado', 3, null),
-	('Retirado', 100.00, '2021/06/02', 'Aprovado', 4, null);
+	('Deposito', 300.00, '2021/06/10 10:52', 'Em aprovação', 2, null),
+	('Deposito', 500.00, '2021/06/02 15:52', 'Aprovado', 3, null),
+	('Retirado', 100.00, '2021/06/02 17:18', 'Aprovado', 4, null);
 
 select * from transacao t;
-	
+
+SELECT conteudo.tutor_id, aulaindividual.aulaindividual_id, usuario.nome as aluno, aulaindividual.dataaulaindividual, 
+    aulaindividual.comentario, conteudo.area_dominio, aulaindividual.duracao from aulaindividual inner join 
+    usuario on usuario.usuario_id = aulaindividual.aluno inner join conteudo on conteudo.conteudo_id = aulaindividual.conteudo where conteudo.tutor_id = 1 and aulaindividual.status = 1;
